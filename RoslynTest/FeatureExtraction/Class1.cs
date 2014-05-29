9f24c121259adba08c3c18a26a889feffd80fbab
GabrieleCocco <gabry.cocco@gmail.com>
2014-05-30 01:07:49 +0200

    First commit. Use Install-Package Microsoft.CodeAnalysis -Pre on the nuget console to install roslyn dlls

---
+++ RoslynTest/FeatureExtraction/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FeatureExtraction
+{
+    public class Class1
+    {
+    }
+}
